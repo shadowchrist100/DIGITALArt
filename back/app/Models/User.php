31bcaf712efdbbbcfb74d578ea30435f8de72354
Abel <lewheabel16@gmail.com>
2026-01-08 13:22:38 +0100

    DB : TABLE USER

--- back/app/Models/User.php
+++ back/app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'nom',
+        'prenom',
         'email',
         'password',
+        'role',
+        'photo_profil'
     ];
 
     /**
