db001584474a6b9419846e0dc7faf916c79eb87a
Abel <lewheabel16@gmail.com>
2026-01-12 18:14:54 +0100

    implementing login and register

--- back/app/Http/Controllers/AuthController.php
+++ back/app/Http/Controllers/AuthController.php
@@ -4,14 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-
+use App\Models\RefreshedToken;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+
 
 class AuthController extends Controller
 {
     public function login(Request $request){
-        $credentials = $request->only('email', 'password');
-        $token =$user = [];
+        $credentials = $request->validate([
+            'email' => ['required', 'email', 'max:50'],
+            'password' => ['required', Password::min(8)]
+        ]);
+        if (! $token = auth('api')->attempt($credentials)) {
+            return response()->json(['error'=> 'Unauthorized'], 401);
+        }
+        $user = User::where('email',$credentials['email'])->first();
+        // return $this->respond_with_token($token,$user);
+        // $credentials = $request->only('email', 'password');
+        // $credentials
+        // $token =$user = [];
 
         // connecté l'utilisateur, lui générer un accestoken 
 
@@ -19,7 +32,17 @@ class AuthController extends Controller
     }
 
     public function register(Request $request){
-        $token =$user = [];
+        $credentials = $request->validate([
+            'nom' => ['required', 'max:50'],
+            'prenom' => ['required', 'max:50'],
+            'email' => ['required', 'email' , 'unique:users', 'max:50'],
+            'password' => ['required', 'confirmed', Password::min(8)],
+            'role' => ['required'],
+            'photo_profil' => ['mimes:jpg,jpeg,svg,png']
+        ]);
+    
+        $user = User::create($credentials);
+        $token = auth('api')->login($user);
 
         // céer l'utilisateur lui générer un accesToken
 
@@ -35,6 +58,21 @@ class AuthController extends Controller
     }
 
     protected function respond_with_token($token, $user){
+        $refresh_token = bin2hex(random_bytes(64));
+        $refresh_token_hash = hash('sha256',$refresh_token);
+        RefreshedToken::create([
+            'user_id' => $user->id,
+            'refreshed_token_hash' => $refresh_token_hash,
+            'expire_at' => now()->addDay(30)
+        ]);
+        $cookie = cookie('refresh_token', $refresh_token, 60 *24*30, '/', null, false, true, false, null );
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user_data' => $user
+        ])->withCookie($cookie);
+
         // récupérer l'accesToken et l'utilisateur 
         // générer un refreshToken 
         // retourner l'utilisateur, l'accesToken 
