528c00de30a86f0d68eb2eb378d460ed7b78f506
LEGBANON Aurelle <aurellelegbanon@gmail.com>
2026-01-18 22:41:17 +0100

    Ajout des tables artisans et ateliers

--- back/routes/api.php
+++ back/routes/api.php
@@ -2,6 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ArtisanController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+
+// Routes pour récupérer les artisans
+Route::get('/artisans', [ArtisanController::class, 'index']);
+Route::get('/artisans/{id}', [ArtisanController::class, 'show']);
